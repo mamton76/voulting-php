56f52327dc213f90ece7aac65466b4e9889b5652
Antonina Mamaeva <mamton@yandex-team.ru>
2017-05-10 19:40:06 +0300

    update

--- add_horse.php
+++ add_horse.php
@@ -7,10 +7,11 @@ echo ' request ';
 echo $_REQUEST['name'];
 echo ' post ';
 echo $_POST['name'];
+echo $_SERVER["CONTENT_TYPE"];
 if ($_SERVER["CONTENT_TYPE"] == 'application/json') {
+    echo ' post json ';
     $postData = file_get_contents('php://input');
     $data = json_decode($postData, true);
-    echo ' post json ';
     echo $data['name'];
 }
 ?>
