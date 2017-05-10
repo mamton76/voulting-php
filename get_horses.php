7785fa430b90028c27a53a4a1da2a61e381cbd00
Antonina Mamaeva <mamton@yandex-team.ru>
2017-05-10 20:25:54 +0300

    update

--- get_horses.php
+++ get_horses.php
@@ -1,22 +1,25 @@
-<!--?php echo 'Привет, мир!'; ?-->
-<!--'{"id":1;"last_update":1;"name":"name"}';-->
 <?php
 include '_db.php';
 include 'config.php';
-//$str = '{"id":1;"last_update":1;"name":"horseName"}';
 
 $sql = SELECT_COMMON_PART . 'name, birth from horse ';
 if ($_GET['name']) {
-    $sql .= " where name like '%".$_GET['name']."%' ";
+    $sql .= " where name like '%" . $_GET['name'] . "%' ";
 }
-echo $sql;
- $res = $DB -> QUR_SEL($sql);
- if ($res) {
+if ($debug) {
+    echo $sql;
+}
+$res = $DB->QUR_SEL($sql);
+if ($res) {
     $str = json_encode($res);
-     echo " data ";
+    if ($debug) {
+        echo " data ";
+    }
     echo $str;
- } else {
-     echo "no data";
- }
+} else {
+    if ($debug) {
+        echo "no data";
+    }
+}
 
 ?>
